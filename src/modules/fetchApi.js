ddf9cecd0c63c3568a9a58665bdc2930e25ae95a
Amaka Konwea <lawrahcollections@gmail.com>
2023-01-12 11:25:59 +0000

    Api update

--- src/modules/fetchApi.js
+++ src/modules/fetchApi.js
@@ -1,9 +1,9 @@
-const url = 'https://www.themealdb.com/api/json/v1/1/search.php?f=c';
+const url = 'https://www.themealdb.com/api/json/v1/1/search.php?f=b';
 
 const getFoodList = async (url) => {
   const response = await fetch(url);
   const json = await response.json();
-  const mealList = json.meals.slice(0, 12);
+  const mealList = json.meals.slice(0, 15);
 
   return mealList;
 };
